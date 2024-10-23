2555e115d2ad2c7a2c9a284309c935c136e7333f
Visharad007 <117433856+Visharad007@users.noreply.github.com>
2024-10-23 14:32:13 +0530

    setup.py

---
+++ forest_theme/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+# get version from __version__ variable in whitetheme_v13/__init__.py
+from forest_theme import __version__ as version
+
+setup(
+	name='forest_theme',
+	version=version,
+	description='forest theme for erpnext',
+	author='Visharad',
+	author_email='vish@gmail.com',
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
